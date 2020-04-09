e5ae5131bd0cb6a617f2e1e49ca26bcecfd85a25
duyuqian <15738381799@163.com>
2020-04-09 11:08:53 +0800

    [duyuqian] feat: create the JDBCUtils class and jdbc properties file for connect mysql

---
+++ src/main/java/util/JDBCUtils.java
@@ -0,0 +1,52 @@
+package util;
+
+import java.io.InputStream;
+import java.sql.*;
+import java.util.Properties;
+
+public class JDBCUtils {
+    public static Connection getConnection() {
+        try {
+            InputStream inputStream = ClassLoader.getSystemClassLoader().getResourceAsStream("jdbc.properties");
+            Properties properties = new Properties();
+            properties.load(inputStream);
+            String url = properties.getProperty("url");
+            String user = properties.getProperty("user");
+            String password = properties.getProperty("password");
+            String driverClass = properties.getProperty("driver");
+            Class.forName(driverClass);
+            return DriverManager.getConnection(url, user, password);
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void closeResource(Connection conn, Statement preparedStatement) {
+        try {
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void closeResource(Connection conn, Statement preparedStatement, ResultSet resultSet) {
+        try {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        closeResource(conn, preparedStatement);
+    }
+}
