efd324256a858647afc89c22ebfce5b9924f9619
duyuqian <15738381799@163.com>
2020-04-09 11:31:28 +0800

    [duyuqian] feat: create the InvalidInputException class and complete the init operate

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,56 +1,84 @@
+import exception.InvalidInputException;
+import exception.InvalidTicketException;
+import exception.ParkingLotFullException;
+import repositories.ParkingLotRepository;
+import repositories.ParkingSpaceRepository;
+
 import java.util.Scanner;
 
 public class Application {
+    private static ParkingLotRepository parkingLotRepository = new ParkingLotRepository();
+    private static ParkingSpaceRepository parkingSpaceRepository = new ParkingSpaceRepository();
 
-  public static void main(String[] args) {
-    operateParking();
-  }
-
-  public static void operateParking() {
-    while (true) {
-      System.out.println("1. 初始化停车场数据\n2. 停车\n3. 取车\n4. 退出\n请输入你的选择(1~4)：");
-      Scanner printItem = new Scanner(System.in);
-      String choice = printItem.next();
-      if (choice.equals("4")) {
-        System.out.println("系统已退出");
-        break;
-      }
-      handle(choice);
+    public static void main(String[] args) {
+        operateParking();
     }
-  }
 
-  private static void handle(String choice) {
-    Scanner scanner = new Scanner(System.in);
-    if (choice.equals("1")) {
-      System.out.println("请输入初始化数据\n格式为\"停车场编号1：车位数,停车场编号2：车位数\" 如 \"A:8,B:9\"：");
-      String initInfo = scanner.next();
-      init(initInfo);
+    public static void operateParking() {
+        while (true) {
+            System.out.println("1. 初始化停车场数据\n2. 停车\n3. 取车\n4. 退出\n请输入你的选择(1~4)：");
+            Scanner printItem = new Scanner(System.in);
+            String choice = printItem.next();
+            if (choice.equals("4")) {
+                System.out.println("系统已退出");
+                break;
+            }
+            handle(choice);
+        }
     }
-    else if (choice.equals("2")) {
-      System.out.println("请输入车牌号\n格式为\"车牌号\" 如: \"A12098\"：");
-      String carInfo = scanner.next();
-      String ticket = park(carInfo);
-      String[] ticketDetails = ticket.split(",");
-      System.out.format("已将您的车牌号为%s的车辆停到%s停车场%s号车位，停车券为：%s，请您妥善保存。\n", ticketDetails[2], ticketDetails[0], ticketDetails[1], ticket);
+
+    private static void handle(String choice) {
+        Scanner scanner = new Scanner(System.in);
+        if (choice.equals("1")) {
+            System.out.println("请输入初始化数据\n格式为\"停车场编号1：车位数,停车场编号2：车位数\" 如 \"A:8,B:9\"：");
+            String initInfo = scanner.next();
+            try {
+                init(initInfo);
+            } catch (InvalidInputException e) {
+                System.out.println(e.getMessage());
+            }
+        } else if (choice.equals("2")) {
+            System.out.println("请输入车牌号\n格式为\"车牌号\" 如: \"A12098\"：");
+            String carInfo = scanner.next();
+            try {
+                String ticket = park(carInfo);
+                String[] ticketDetails = ticket.split(",");
+                System.out.format("已将您的车牌号为%s的车辆停到%s停车场%s号车位，停车券为：%s，请您妥善保存。\n", ticketDetails[2], ticketDetails[0], ticketDetails[1], ticket);
+            } catch (ParkingLotFullException e) {
+                System.out.println(e.getMessage());
+            }
+        } else if (choice.equals("3")) {
+            System.out.println("请输入停车券信息\n格式为\"停车场编号1,车位编号,车牌号\" 如 \"A,1,8\"：");
+            String ticket = scanner.next();
+            try {
+                String car = fetch(ticket);
+                System.out.format("已为您取到车牌号为%s的车辆，很高兴为您服务，祝您生活愉快!\n", car);
+            } catch (InvalidTicketException e) {
+                System.out.println(e.getMessage());
+            } catch (InvalidInputException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
-    else if (choice.equals("3")) {
-      System.out.println("请输入停车券信息\n格式为\"停车场编号1,车位编号,车牌号\" 如 \"A,1,8\"：");
-      String ticket = scanner.next();
-      String car = fetch(ticket);
-      System.out.format("已为您取到车牌号为%s的车辆，很高兴为您服务，祝您生活愉快!\n", car);
+
+    public static void init(String initInfo) {
+        if (!(initInfo.contains(",") && initInfo.contains(":"))) {
+            throw new InvalidInputException("请输入正确的初始化停车场数据");
+        }
+        parkingLotRepository.deleteAllParkingLotInfo();
+        parkingSpaceRepository.deleteAllParkingSpaceInfo();
+        for (String parkingLotInfo : initInfo.split(",")) {
+            String[] parkingLotInfoArr = parkingLotInfo.split(":");
+            parkingLotRepository.initParkingLotInfo(parkingLotInfoArr[0], Integer.parseInt(parkingLotInfoArr[1]));
+        }
     }
-  }
 
-  public static void init(String initInfo) {
+    public static String park(String carNumber) {
+        return "";
+    }
 
-  }
-
-  public static String park(String carNumber) {
-    return "";
-  }
-
-  public static String fetch(String ticket) {
-    return "";
-  }
+    public static String fetch(String ticket) {
+        return "";
+    }
 
 }
---
+++ src/main/java/exception/InvalidInputException.java
@@ -0,0 +1,12 @@
+package exception;
+
+public class InvalidInputException extends RuntimeException {
+    public InvalidInputException() {
+        super();
+    }
+
+    public InvalidInputException(String message) {
+        super(message);
+    }
+
+}
